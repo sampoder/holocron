e6408cf12772b637e6268b22ced21584f1c61937
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-12-26 17:11:52 +0800

    Updates

---
+++ precache-manifest.fbd712cbc382578fa3edf31e4d5bb40f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b0091d578bee00ffdc7d6b7b18ff01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a87ff9d994fc0fcd4aaa",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "38f5b542c8a8ccf1def9",
+    "url": "./static/js/2.289562b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.289562b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87ff9d994fc0fcd4aaa",
+    "url": "./static/js/main.593286e3.chunk.js"
+  },
+  {
+    "revision": "a00306a2b682d274b3c4",
+    "url": "./static/js/runtime-main.550f24de.js"
+  }
+]);
