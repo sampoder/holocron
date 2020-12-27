eba019d9284ceb4f50b89fb94b45d9c633a2e03b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-12-27 11:17:39 +0800

    smh

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,31 +1,36 @@
-import './App.css'
-import Holocron from '@sampoder/holocron'
+import "./App.css";
+import Holocron from "@sampoder/holocron";
 
-function Title(){
-  return <h1 style={{ color: 'white' }}>The Hello World!</h1>
+function Title() {
+  return <h1 style={{ color: "white" }}>The Hello World!</h1>;
 }
 
-function LaunchButton(){
-  return <button style={{ color: 'white' }}>Play</button>
+function LaunchButton() {
+  return <button style={{ color: "white" }}>Play</button>;
 }
 
-function FullscreenButton(){
-  return <button style={{ color: 'white' }}>Fullscreen</button>
+function FullscreenButton() {
+  return <button style={{ color: "white" }}>Fullscreen</button>;
 }
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Holocron
         title={<Title />}
-        backgroundColor='#8492a6'
+        backgroundColor="#8492a6"
         launch={<LaunchButton />}
         fullscreen={<FullscreenButton />}
       >
-        <p style={{ color: 'white'}}><img src="https://cloud-oyha4bho9.vercel.app/0screenshot_20201227-111151_jedi_challenges.jpg" /></p>
+        <p style={{ color: "white" }}>
+          <img
+            src="https://cloud-oyha4bho9.vercel.app/0screenshot_20201227-111151_jedi_challenges.jpg"
+            alt="Jedi Challenges Setup"
+          />
+        </p>
       </Holocron>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
