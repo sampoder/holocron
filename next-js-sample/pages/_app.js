2c645a4d2ff09eed3975318ec81365583b1a2560
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-12-27 10:54:06 +0800

    Remove Next.js Sample

--- next-js-sample/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
